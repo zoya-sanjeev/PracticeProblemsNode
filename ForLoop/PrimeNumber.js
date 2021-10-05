a9cb71063425e1c17cef1ba49981ca406a2b20e2
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-05 22:47:37 +0530

    MODIFIED: find prie numbers in given range

--- ForLoop/PrimeNumber.js
+++ ForLoop/PrimeNumber.js
@@ -2,15 +2,22 @@ var readline = require('readline');
 var rl = readline.createInterface(
      process.stdin, process.stdout);
 var flag=0;
-rl.question('Enter number ', (num) => {
-    for(let i =2; i<num; i++){
-        if(num%i==0){
+rl.question('Enter number ', (num1) => {
+    rl.question('Enter number ', (num2) => {
+    for(let i =num1; i<=num2; i++){
+        for(let j =2;j<i;j++){
+        if(i%j==0){
             console.log("Not Prime")
             flag=1;
             break;
         }
+    
     }
     if(flag==0)
-        console.log("Prime");
+    console.log("Prime");
+}
+   
     rl.close();
+});
+    
 });
