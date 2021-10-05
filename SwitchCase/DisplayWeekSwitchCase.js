de9555be333b640b509b13934f43a247377e811e
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-05 22:10:25 +0530

    ADDED: program to display day of the week from number'

---
+++ SwitchCase/DisplayWeekSwitchCase.js
@@ -0,0 +1,24 @@
+var readline = require('readline'); 
+var rl = readline.createInterface(
+     process.stdin, process.stdout);
+
+rl.question('Enter week number ', (num) => {
+    switch(Number(num)){
+        case 1: console.log("Sunday");
+                break;
+        case 2: console.log("Monday");
+                break;
+        case 3: console.log("Tuesday");
+                break;
+        case 4: console.log("Wednesday");
+                break;
+        case 5: console.log("Thursday");
+                break;
+        case 6: console.log("Friday");
+                break;
+        case 7: console.log("Saturday");
+                break;
+        default: console.log("Enter single digit");
+    }
+    rl.close();
+});
