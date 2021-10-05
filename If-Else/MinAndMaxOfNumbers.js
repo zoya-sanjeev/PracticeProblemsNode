7c3683faabc227810c7220358f7f4510a315dc18
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-05 19:41:44 +0530

    REFACTORED: implemented max and min using if else

--- If-Else/MinAndMaxOfNumbers.js
+++ If-Else/MinAndMaxOfNumbers.js
@@ -9,5 +9,25 @@ let num4= getRandomNumber();
 let num5= getRandomNumber();
 
 console.log("The numbers are: "+num1+ ","+num2+ ","+num3+ ","+num4+ ","+num5);
-console.log("Minimum number is :"+ Math.min(num1,num2,num3,num4,num5));
-console.log("Maximum number is :"+ Math.max(num1,num2,num3,num4,num5));
+let min = num1;
+if(num2< min)
+    min=num2;
+if(num3<min)
+    min=num3
+if(num4<min)
+    min=num4;
+if(num5<min)
+    min=num5;
+
+let max = num1;
+if(num2> max)
+    max=num2;
+if(num3> max)
+    max=num3;
+if(num4> max)
+    max=num4;
+if(num5> max)
+    max=num5;
+
+console.log("Minimum number is :"+min);
+console.log("Maximum number is :"+ max);
