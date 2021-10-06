5f7feb2bb4a667c7c069d957fa806114ccec5888
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-10-07 01:16:27 +0530

    ADDED: logic to store frequency to die rolls

--- Map/DieProblem.js
+++ Map/DieProblem.js
@@ -1,6 +1,13 @@
 let numOfRolls = 5;
 let mapOfDieRolls = new Map();
-for(let i = 0; i<5; i++){
-    roll = Math.ceil(Math.random() * 6);
-    mapOfDieRolls.set(i,roll);
+for(let i = 1; i<=6; i++){
+    mapOfDieRolls.set(i,0);
 }
+while(true){
+    roll = Math.ceil(Math.random()* 6);
+    let count =  mapOfDieRolls.get(roll);
+    mapOfDieRolls.set(roll,count+1);
+    if(count+1 == 10)
+        break;
+}
+console.log(mapOfDieRolls);
